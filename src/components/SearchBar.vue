cdebe430b0d424095990807ca97836b43a8c23fc
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-08-07 16:20:25 +0700

    Implement search bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import Input from './Input.vue';
+import Button from './SearchButton.vue';
+
+const router = useRouter();
+const searchInput = ref<string>('');
+
+const handleSearch = () => {
+  if (searchInput.value) {
+    router.push({ name: 'words', params: { word: searchInput.value } });
+  }
+};
+</script>
+
+<template>
+  <form class="flex relative w-full" @submit.prevent="handleSearch">
+    <Input v-model="searchInput" />
+    <Button @click="handleSearch">Submit</Button>
+  </form>
+</template>
