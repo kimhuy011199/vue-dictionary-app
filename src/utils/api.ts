e67f99ee26ed7c7ea030e105669f60663148c6e5
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-08-18 22:38:19 +0700

    Remove un use files

--- src/utils/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import type { Word } from './type';
-
-const DICTIONARY_API = 'https://api.dictionaryapi.dev/api/v2/entries/en';
-
-export const fetchWord = async (word: string): Promise<Word | undefined> => {
-  const data = await axios.get(`${DICTIONARY_API}/${word}`);
-  return data.data[0];
-};
