37fa5aae253293286167a5951399314d5afe8400
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-08-19 19:00:56 +0700

    Create app background

---
+++ src/components/Background.vue
@@ -0,0 +1,7 @@
+<template>
+  <p
+    class="fixed opacity-15 leading-tight text-right text-9xl font-semibold -rotate-6 top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2"
+  >
+    VUEJS ENGLISH DICTIONARY
+  </p>
+</template>
--- src/components/Meaning.vue
+++
@@ -1,5 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div>Meaning</div>
-</template>
