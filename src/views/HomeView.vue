fb6bafdda512748dc701c89b615a2e3ba6535d84
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-08-23 11:30:03 +0700

    Create home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SearchBar from '@/components/SearchBar.vue';
+</script>
 
 <template>
-  <main></main>
+  <main class="flex flex-col items-center justify-center my-auto z-10 px-5">
+    <div class="flex flex-col">
+      <p class="text-center text-3xl font-semibold pb-4">English Dictionary</p>
+      <p class="text-center text-5xl font-bold pb-8 px-3 leading-tight">"One Word, One Wiki"</p>
+      <SearchBar />
+    </div>
+  </main>
 </template>
