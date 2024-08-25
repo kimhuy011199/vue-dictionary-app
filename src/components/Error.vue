163c23551f5e9db1b56e77ddf6d6c8421b9f9555
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-08-25 23:28:27 +0700

    Error UI

---
+++ src/components/Error.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex w-full mt-5 justify-center items-center">
+    <span class="text-xl">Something went wrong. Please try again.</span>
+  </div>
+</template>
---
+++ src/components/WordNotFound.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  word: string;
+}>();
+</script>
+
+<template>
+  <div class="flex w-full mt-5 justify-center items-center">
+    <div class="text-xl">
+      <span>No results found:&nbsp;</span>
+      <span class="text-3xl font-semibold">{{ word }}</span>
+    </div>
+  </div>
+</template>
