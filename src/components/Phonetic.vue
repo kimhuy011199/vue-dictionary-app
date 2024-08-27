e06edbd28d0180dfcfaf5daeae2de6c7d2116038
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-08-27 21:31:16 +0700

    Handle phonetic sound

--- src/components/Phonetic.vue
+++ src/components/Phonetic.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
+import type { Phonetic } from '@/models/Word';
 import PlayButton from './PlayButton.vue';
+
+defineProps<{
+  phonetic: Phonetic;
+  word: string;
+}>();
 </script>
 
 <template>
-  <div class="flex justify-between">
+  <div class="flex justify-between items-center px-7 py-5">
     <div class="flex flex-col">
-      <span class="font-bold">Phonetic</span>
-      <span> /fəˈnɛtɪk/</span>
+      <span class="text-black text-3xl pb-1 font-semibold">{{ word }}</span>
+      <span class="text-primary text-lg">{{ phonetic.text }}</span>
     </div>
-    <PlayButton />
+    <PlayButton :audio="phonetic.audio" />
   </div>
 </template>
