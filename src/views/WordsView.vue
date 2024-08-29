4d79087155ecda69265194662b896c5db2f344b1
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-08-29 09:48:28 +0700

    Optimize wordsview

--- src/views/WordsView.vue
+++ src/views/WordsView.vue
@@ -1,65 +1,23 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue';
-import { useRoute } from 'vue-router';
-import { fetchWord } from '@/utils/api';
-import { HttpStatus } from '@/utils/enum';
 import SearchBar from '@/components/SearchBar.vue';
 import Result from '@/components/Result.vue';
-import Header from '@/components/Header.vue';
-import type { Word } from '@/utils/type';
+import Loading from '@/components/Loading.vue';
+import Error from '@/components/Error.vue';
+import WordNotFound from '@/components/WordNotFound.vue';
+import { useRouteParams } from '@/composables/useRouteParams';
+import { useLookupWord } from '@/composables/useLookupWord';
+import { FetchStatus } from '@/utils/enum';
 
-const route = useRoute();
-const word = route.params.word as string;
-
-const result = ref<Word | undefined>(undefined);
-const isError = ref<boolean>(false);
-const isLoading = ref<boolean>(false);
-const isNotFound = ref<boolean>(false);
-
-const lookupWord = async (word: string) => {
-  // Reset states
-  isLoading.value = true;
-  isNotFound.value = false;
-  isError.value = false;
-  result.value = undefined;
-
-  try {
-    const data = await fetchWord(word);
-    if (data) {
-      console.log('data', data);
-      result.value = data;
-    } else {
-      isNotFound.value = true;
-    }
-  } catch (error: any) {
-    if (error?.response?.status === HttpStatus.NOT_FOUND) {
-      isNotFound.value = true;
-    } else {
-      isError.value = true;
-    }
-  } finally {
-    isLoading.value = false;
-  }
-};
-
-watch(
-  () => route.params.word,
-  (newWord) => {
-    lookupWord(newWord as string);
-  }
-);
-
-onMounted(() => {
-  lookupWord(word);
-});
+const word = useRouteParams('word');
+const { wordData, error, status, isNotFound } = useLookupWord(word);
 </script>
 
 <template>
-  <main>
-    <div class="mx-auto max-w-2xl w-full">
-      <Header />
-      <SearchBar />
-      <Result v-if="result" :result="result" />
-    </div>
-  </main>
+  <div class="z-10 mx-auto max-w-2xl w-full mb-auto px-5 md:px-0">
+    <SearchBar />
+    <Loading v-if="status === FetchStatus.FETCHING" />
+    <Result v-if="status === FetchStatus.RESOLVED && wordData" :result="wordData" />
+    <Error v-if="status === FetchStatus.REJECTED && error" />
+    <WordNotFound v-if="isNotFound" :word="word" />
+  </div>
 </template>
